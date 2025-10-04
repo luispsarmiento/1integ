eb80604d62dbc626c94561758dac33b2370f4ac9
Luis Puc <contacto@luispsarmiento.com>
2025-10-04 01:23:30 -0600

    Added: tailwind configuration

---
+++ OneInteg.Client/tailwind.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.{razor,html}', // Adjust path as needed for your project structure
+        './wwwroot/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
